7c10266703de6f0e0a6655a579f5c607f35f1f7b
Vinay.Kumar <Vinay.Kumar@target.com>
2020-09-30 06:40:03 +0530

    array rearrangment

---
+++ src/main/java/datastructure/array/arragement_rearrangement/EvenIndexSmallerValueAndOddIndexGreater.java
@@ -0,0 +1,35 @@
+package datastructure.array.arragement_rearrangement;
+
+import java.util.Arrays;
+
+public class EvenIndexGreateValueAndOddIndexSmaller {
+    public static void main(String[] args) {
+        int a[] = {4,3,2,6,7,8,9};
+        reArrange(a);
+        System.out.println(Arrays.toString(a));
+    }
+    public static void reArrange(int[] a)
+    {
+        int temp;
+        int n = a.length;
+        for(int i=0;i<n-2;i++)
+        {
+            if( i % 2 ==0 && a[i] > a[i+1])
+            {
+               swap(i,i+1,a);
+            }
+            if( i%2 != 0 && a[i]<a[i+1])
+            {
+                swap(i,i+1,a);
+            }
+        }
+    }
+
+    public static void swap( int current, int next,int[] a)
+    {
+        int temp;
+        temp = a[current];
+        a[current] = a[next];
+        a[next] = temp;
+    }
+}
---
+++ src/main/java/datastructure/array/arragement_rearrangement/Sagregate0s1s.java
@@ -0,0 +1,2 @@
+package datastructure.array.arragement_rearrangement;public class Sagregate0s1s {
+}
